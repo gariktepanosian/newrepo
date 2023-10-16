5bcd2203ef746b999f33618487ffb7d727563e92
Garik <gariktepanosian@gmail.com>
2023-10-16 17:30:29 +0400

    new issue test

--- src/test/java/com/example/initing/InitingApplicationTests.java
+++ src/test/java/com/example/initing/InitingApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.initing;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = Api.class)
-class InitingApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.initing;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest(classes = Api.class)
+//class InitingApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
